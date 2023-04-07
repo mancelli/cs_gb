5e1c2b418176cd004ff99f4dbdd11386d237a81a
Mikhail Iagodin <dnshade19@gmail.com@>
2023-04-07 20:45:04 +0500

    added cs3 folder

--- cs3/Program.cs
+++ cs3/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int numberA = 6;
+int numberB = 7;
+int result = numberA + numberB;
+Console.WriteLine("Результат равен ");
+Console.WriteLine(result);
