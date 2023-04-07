3ecd180a8fffe64aa5c86eeda105cc9c60318505
Mikhail Iagodin <dnshade19@gmail.com@>
2023-04-07 23:20:53 +0500

    first semi second task added

---
+++ cs_semi2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+// Например:
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b*b)
+{
+    Console.WriteLine("Число 1 является квадратом числа 2");
+}
+else
+{ 
+    if (b == a*a)
+    {
+        Console.WriteLine("Число 2 является квадратом числа 1");
+    }
+    else
+    {
+        Console.WriteLine("Несовместимые числа");
+    }
+}
