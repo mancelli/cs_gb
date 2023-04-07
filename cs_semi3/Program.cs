05fd78865f8c423574217b7fec93053516693899
Mikhail Iagodin <dnshade19@gmail.com@>
2023-04-07 23:34:19 +0500

    first semi third task added

---
+++ cs_semi3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+// Например:
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49
+
+Console.WriteLine("Введите номер дня недели");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (day == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (day == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (day == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (day == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (day == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (day == 7)
+{
+    Console.WriteLine("Воскресенье");
+}   
+else
+{
+    Console.WriteLine("Некорректное число");
+}
