d5e96558eeafa434f08654f3556f41f4adfa06cc
Mikhail Iagodin <dnshade19@gmail.com@>
2023-04-07 21:04:42 +0500

    added cs6 folder and maxweight program

---
+++ cs6/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 2;
+int b = 6;
+int c = 5;
+int d = 7;
+int e = 6;
+int max = 0;
+
+if (a > max) max = a; 
+if (b > max) max = b; 
+if (c > max) max = c; 
+if (d > max) max = d; 
+if (e > max) max = e; 
+
+
+Console.WriteLine(max);
