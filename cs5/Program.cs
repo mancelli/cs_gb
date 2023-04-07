ef948b5c57c7e89f6ad05bbe1f79413972fb3dbf
Mikhail Iagodin <dnshade19@gmail.com@>
2023-04-07 20:56:56 +0500

    added cs5 folder with helloer program

---
+++ cs5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Веедите свое имя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "миша")
+{
+    Console.WriteLine("Ура, это же Миша!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
