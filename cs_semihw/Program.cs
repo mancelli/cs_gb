0d24d34065a32ffec8548307862c003eb94435ff
Mikhail Iagodin <dnshade19@gmail.com@>
2023-04-08 21:49:39 +0500

    ended first semi hw

--- cs_semihw/Program.cs
+++ cs_semihw/Program.cs
@@ -16,22 +16,58 @@
 
 //программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
 
-Console.WriteLine("Введите первое число");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-int num2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите третье число");
-int num3 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите первое число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число");
+// int num3 = Convert.ToInt32(Console.ReadLine());
 
-int max = num1;
-if (max < num2)
-{
-    max = num2;
-}
-if (max < num3)
-{
-    max = num3;
-}
+// int max = num1;
+// if (max < num2)
+// {
+//     max = num2;
+// }
+// if (max < num3)
+// {
+//     max = num3;
+// }
 
-Console.Write("Вот наибольшее число:");
-Console.WriteLine(max);
+// Console.Write("Вот наибольшее число:");
+// Console.WriteLine(max);
+
+
+// программу, которая на вход принимает число и выдаёт, является ли число чётным
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int even = num%2;
+// if (even > 0)
+// {
+//     Console.WriteLine("Число нечетное");
+// }
+// else
+// {
+//     Console.WriteLine("Число четное");
+// }
+
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int start = 1;
+// int even = start%2;
+
+// while (start <= num)
+// {
+//     if (even == 0)
+//     {
+//         Console.Write(start);
+//         Console.Write(" ");
+//     }
+//     start = start + 1;
+//     even = start%2;
+// }
+
+// Console.WriteLine("");
+// Console.WriteLine("Вот все четные числа до введенного числа от единицы");
