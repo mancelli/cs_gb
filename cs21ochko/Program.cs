c8a632de20de200aa4af60912bb2cfa7ff58648e
Mikhail Iagodin <dnshade19@gmail.com@>
2023-04-15 23:32:58 +0500

    minor bug fixes

--- cs21ochko/Program.cs
+++ cs21ochko/Program.cs
@@ -1,4 +1,8 @@
 ﻿
+
+
+
+
 int[] cards = {1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,11,11,11,11,12,12,12,12,13,13,13,13};
 
 int[] dealer = new int[5];
@@ -27,8 +31,11 @@ for (int i = 0; i<5; i++)
     cards[index] = 0;
     while (user[i] == 0)
     {
-        user[i] = cards[index+1];
-        if (index>51)
+        if (index<=51)
+        {
+            user[i] = cards[index+1];
+        }
+        else if (index>51)
         {
             index = 0;
         }
@@ -51,14 +58,19 @@ Console.WriteLine("Карты дилера ");
 Console.Write(dealer[0]);
 Console.Write(" " + dealer[1]);
 Console.ReadLine();
+int sumdealer = dealer[0]+dealer[1];
 Console.WriteLine();
 Console.WriteLine("Твои карты ");
 Console.Write(user[0]);
 Console.Write(" " + user[1]);
-Console.ReadLine();
-Console.WriteLine("Ход дилера!");
-int sumdealer = dealer[0]+dealer[1];
 int sumuser = user[0]+user[1];
+Console.ReadLine();
+if (sumuser>21)
+{
+    Console.WriteLine();
+    Console.WriteLine("У вас перебор! Увы, вы проиграли!");
+}
+Console.WriteLine("Ход дилера!");
 Console.WriteLine("Карты дилера ");
 Console.Write(dealer[0]);
 Console.Write(" " + dealer[1]);
@@ -72,7 +84,7 @@ while (sumdealer < 17 && count < 5)
 Console.WriteLine();
 if (sumdealer>21)
 {
-    Console.WriteLine("Перебор! Вы выиграли!");
+    Console.WriteLine("Перебор у дилера! Вы выиграли!");
     Console.WriteLine();
 }
 else if (sumdealer>=17 && sumdealer<21)
@@ -83,10 +95,10 @@ else if (sumdealer>=17 && sumdealer<21)
     Console.WriteLine();
     Console.WriteLine("Твои карты ");
     Console.Write(user[0]);
-    Console.Write(" " + user[1]);
+    Console.Write(" " + user[1] + " ");
     while (choice == "Y" && sumuser<21 && i<5)
     {
-        Console.Write(" " + user[i]);
+        Console.Write(user[i]+ " ");
         sumuser = sumuser + user[i];
         i++;
         if (sumuser>21)
