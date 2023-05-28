90e110888790a651da460dfe157ca2fac47f2082
Mikhail Iagodin <dnshade19@gmail.com@>
2023-05-28 10:38:49 +0500

    finished with ackerman func

--- csattestationwork/Program.cs
+++ csattestationwork/Program.cs
@@ -10,7 +10,7 @@ void PrintToLesser(int N)
     PrintToLesser(N-1);    
 }
 
-PrintToLesser(5);
+// PrintToLesser(5);
 
 // Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 System.Console.WriteLine();
@@ -27,6 +27,21 @@ void SummaryByArray(int M, int N)
     SummaryByArray(M+1,N);
 }
 
-SummaryByArray(1,4);
+// SummaryByArray(1,4);
 
 
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int A(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (m > 0 && n == 0)
+        return A(m - 1, 1);
+    else
+        return A(m - 1, A(m, n - 1));
+}
+
+// System.Console.WriteLine(A(2,3));
+
+// System.Console.WriteLine(A(3,2));
